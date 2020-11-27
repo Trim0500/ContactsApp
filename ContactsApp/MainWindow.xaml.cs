5f8dd01fa3c32a488530902bef0845e0b852c13f
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2020-11-27 15:37:13 -0500

    Established rough outline of look and functionality

--- ContactsApp/MainWindow.xaml.cs
+++ ContactsApp/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ContactsApp.BehindCodeClasses;
 
 namespace ContactsApp
 {
@@ -23,6 +24,47 @@ namespace ContactsApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<ContactsBinding> listContento = new List<ContactsBinding>();
+
+            //listContento.Add(new ContactsBinding() { ContactInfo = "This is text" });
+
+            //ContactsListItems.ItemsSource = listContento;
+
+            for (int i = 0; i < 20; i++)
+            {
+                listContento.Add(new ContactsBinding() { ContactInfo = "This is a test" });
+            }
+
+            ContactsListItems.ItemsSource = listContento;
+        }
+
+        private void ListBoxItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("This is a test");
+        }
+
+        private void Add_Contact_btn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This is a test");
+        }
+
+        private void Edit_Contact_btn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This is a test");
+        }
+
+        private void Del_Contact_btn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This is a test");
+        }
+
+        private void Imp_Contact_btn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This is a test");
         }
     }
 }
